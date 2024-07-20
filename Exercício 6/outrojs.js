4200cd3631c9916047a24c3ad3d1ebe060d69bb1
Gabriel Bueno Jacinto <131608321+AlgolGB@users.noreply.github.com>
2024-07-20 10:29:39 -0300

    Add files via upload

--- Exercício 6/outrojs.js
+++ Exercício 6/outrojs.js
@@ -2,9 +2,13 @@ const frm = document.querySelector("form");
 const resp1 = document.querySelector("#lista");
 const resp2 = document.querySelector("#outLista");
 
-const compras = [" tomate", " pepino", " frango", " pasta dental", " café", " arroz", " feijão", " alface", " sabão em pó", " massa de tomate"];
+const compras = ["tomate", "pepino", "frango", "pasta dental", "café","arroz", "feijão", "alface", "sabão em pó", "massa de tomate"];
 
-resp1.innerText = compras;
+resp1.innerText =  compras.join("\n");
+
+//compras.forEach((produto, i) => {
+//    resp1.innerText =  (i+1) + 'º Produto: ' + produto
+//});
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -16,7 +20,7 @@ frm.addEventListener("submit", (e) => {
 
     const itens = compras.splice(0, num);
     
-    resp2.innerText = compras;
+    resp2.innerText = compras.join('\n');
     
     document.getElementById("inNum").value = "";
 });
