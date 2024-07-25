fe206a7458d015bd305e97d42abe9931480c1a99
Gabriel Bueno Jacinto <131608321+AlgolGB@users.noreply.github.com>
2024-07-25 19:12:55 -0300

    Add files via upload

--- Exercício 11/arrayobjetos.js
+++ Exercício 11/arrayobjetos.js
@@ -11,7 +11,8 @@ frm.addEventListener("submit", (e) => {
     //const age = Number(frm.inIdade.value)
 
     const pessoas2 = pessoas.map(aux => ({name: aux.name, nasc: 2024 - aux.age}))
-    const pessoas3 = pessoas.filter(aux => aux.age <= 18 && aux.name.includes("z"))
+    //const pessoas3 = pessoas.filter(aux => aux.age <= 18 && aux.name.includes("z"))
+    const pessoas4 = pessoas.reduce((a, pessoa) => Math.max(a,pessoa.age),0)
 
     let  resposta = ""
     let filtro = ""
@@ -20,13 +21,14 @@ frm.addEventListener("submit", (e) => {
         resposta = resposta + `${pessoa.name}, Nasceu em ${pessoa.nasc} \n`
         resp.innerText = resposta
     }
-    for(const pessoa of pessoas3){
-            filtro += pessoa.name + "\n"
-            resp1.innerText = filtro
-    }
-    if(pessoas3.length == 0){
-        resp1.innerText = `Não há pessoas com condições desejadas`
-    }
+    //for(const pessoa of pessoas3){
+    //        filtro += pessoa.name + "\n"
+    //        resp1.innerText = filtro
+    //}
+    //if(pessoas3.length == 0){
+    //    resp1.innerText = `Não há pessoas com condições desejadas`
+    //}
 
+    resp1.innerText = `A maior idade é ${pessoas4}`
     
 })
