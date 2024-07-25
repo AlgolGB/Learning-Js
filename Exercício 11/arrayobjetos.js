53a4859fd7b64ffc289d2e76f9955d30c6f03a11
Gabriel Bueno Jacinto <131608321+AlgolGB@users.noreply.github.com>
2024-07-24 22:21:05 -0300

    Add files via upload

--- Exercício 11/arrayobjetos.js
+++ Exercício 11/arrayobjetos.js
@@ -1,7 +1,8 @@
 const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const resp = document.querySelector("#outAno")
+const resp1 = document.querySelector("#outC")
 
-const pessoas = [{name: "Yago", age: 30}, {name: "José", age: 35}, {name: "Antônio", age: 67} ]
+const pessoas = [{name: "Yago", age: 30}, {name: "José", age: 35}, {name: "Antônio", age: 67}, {name: "Lucas", age: 9}, {name: "Jéssica", age: 17}, {name: "Rodrigo", age: 12}, {name: "Marcelo", age: 12}]
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -10,13 +11,22 @@ frm.addEventListener("submit", (e) => {
     //const age = Number(frm.inIdade.value)
 
     const pessoas2 = pessoas.map(aux => ({name: aux.name, nasc: 2024 - aux.age}))
+    const pessoas3 = pessoas.filter(aux => aux.age <= 18 && aux.name.includes("z"))
 
     let  resposta = ""
+    let filtro = ""
 
     for(const pessoa of pessoas2){
-
         resposta = resposta + `${pessoa.name}, Nasceu em ${pessoa.nasc} \n`
         resp.innerText = resposta
     }
+    for(const pessoa of pessoas3){
+            filtro += pessoa.name + "\n"
+            resp1.innerText = filtro
+    }
+    if(pessoas3.length == 0){
+        resp1.innerText = `Não há pessoas com condições desejadas`
+    }
+
     
 })
